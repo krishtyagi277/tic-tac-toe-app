27ef1ad66b0df4cfb2023d598ccab05719297a28
krishna tyagi <35443292+krishtyagi277@users.noreply.github.com>
2020-08-30 17:19:48 +0530

    Updates

---
+++ precache-manifest.0fbb17b5274aa415606dd8e7b3fcb658.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e5ddcffe5851ae9e6b90840766bd702",
+    "url": "/tic-tac-toe-app/index.html"
+  },
+  {
+    "revision": "d0ee0864cd524b4abaa2",
+    "url": "/tic-tac-toe-app/static/css/2.1f35d23a.chunk.css"
+  },
+  {
+    "revision": "b7bd64f18ba0b916790c",
+    "url": "/tic-tac-toe-app/static/css/main.96eedbb3.chunk.css"
+  },
+  {
+    "revision": "d0ee0864cd524b4abaa2",
+    "url": "/tic-tac-toe-app/static/js/2.78afbebd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tic-tac-toe-app/static/js/2.78afbebd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7bd64f18ba0b916790c",
+    "url": "/tic-tac-toe-app/static/js/main.b7ab6d78.chunk.js"
+  },
+  {
+    "revision": "a6737ef5769a092390f9",
+    "url": "/tic-tac-toe-app/static/js/runtime-main.3d217d62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
